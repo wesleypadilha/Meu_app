dfce991272cf41aceef78ccae04d75771be93987
wesleypadilha <wtpadilhaa@gmail.com>
2024-04-22 16:35:22 -0300

    Ajustes

--- Gemfile
+++ Gemfile
@@ -69,3 +69,5 @@ group :test do
 end
 
 gem 'pagy'
+
+gem 'webpacker'
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_19_172148) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_22_192826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "content"
+    t.text "body"
+    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -25,11 +25,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_19_172148) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "content", null: false
-    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
